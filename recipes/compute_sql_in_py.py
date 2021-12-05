a6ec8b90465f357667fed3b94e7152eb842932e6
admin <admin>
2021-12-05 11:15:10 +0100

    Recipe 2_AUT_EG.compute_sql_in_py saved through notebook notebook editor for compute_sql_in_py

--- recipes/compute_sql_in_py.py
+++ recipes/compute_sql_in_py.py
@@ -17,15 +17,17 @@ synthetic_data2_prepared = dataiku.Dataset("synthetic_data2_prepared")
 synthetic_data2_prepared_df = synthetic_data2_prepared.get_dataframe()
 
 which_query = int(dataiku.get_custom_variables()['which_query'])
+which_query
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 if which_query == 1:
     query = f"""SELECT *
                 FROM "{project_key}_synthetic_data2_prepared"
                 LIMIT(1) """
-elif which_query == 1:
-    query = f"""SELECT * 
-                FROM "{project_key}_synthetic_data2_prepared" 
-                LIMIT(100) """
+elif which_query == 2:
+    query = f"""SELECT *
+                FROM "{project_key}_synthetic_data2_prepared"
+                LIMIT(2) """
 
 sql_in_py_df = executor.query_to_df(query)
 
