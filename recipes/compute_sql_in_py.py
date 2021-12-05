f5ce4526129a570c7eda23e4121236b25b9347f5
admin <admin>
2021-12-05 11:06:12 +0100

    Recipe 2_AUT_EG.compute_sql_in_py saved through notebook notebook editor for compute_sql_in_py

--- recipes/compute_sql_in_py.py
+++ recipes/compute_sql_in_py.py
@@ -1,20 +1,38 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+from dataiku import SQLExecutor2, Dataset
+from dataikuapi import DSSClient
+
+
+client = dataiku.api_client()
+project_key = dataiku.get_custom_variables()['projectKey']
+executor = SQLExecutor2(connection="test_connection")
+
 
 # Read recipe inputs
 synthetic_data2_prepared = dataiku.Dataset("synthetic_data2_prepared")
 synthetic_data2_prepared_df = synthetic_data2_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+which_query = int(dataiku.get_custom_variables()['which_query'])
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+if which_query == 1:
+    query = f"""SELECT * 
+                FROM "{project_key}_synthetic_data2_prepared" 
+                LIMIT(1) """
+elif which_query == 1:
+    query = f"""SELECT * FROM "{project_key}_synthetic_data2_prepared" LIMIT(1) """
 
-sql_in_py_df = synthetic_data2_prepared_df # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+sql_in_py_df = executor.query_to_df(query)
 
+sql_in_py_df
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 sql_in_py = dataiku.Dataset("sql_in_py")
 sql_in_py.write_with_schema(sql_in_py_df)
